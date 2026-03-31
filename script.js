4d1665e779c198242ee3ca7ddfc58bb4345d2b4a
taqiyyhdh <taqiyyhdh@gmail.com>
2026-03-31 19:59:54 +0700

    ✨ feat: refactor menu list to fetch data from API

---
+++ script.js
@@ -0,0 +1,35 @@
+let allMenu = [];
+
+async function fetchMenu(keyword = '') {
+  try {
+    const response = await fetch('https://wpu-cafe.vercel.app/api/menu?search=' + keyword);
+    const data = await response.json();
+    allMenu = data.data;
+    displayMenu(allMenu);
+  } catch (error) {
+    console.error('Error fetching menu:', error);
+  }
+}
+
+fetchMenu(); // Panggil fungsi untuk mengambil data menu
+
+function displayMenu(menu) {
+  const menuList = document.getElementById('menu-list');
+  menuList.innerHTML = '';
+
+  menu.forEach((item) => {
+    const card = document.createElement('div');
+    card.classList.add('menu-card');
+
+    const image = document.createElement('img');
+    image.src = item.image_url;
+    image.alt = item.name;
+    card.appendChild(image);
+
+    const name = document.createElement('h2');
+    name.textContent = item.name;
+    card.appendChild(name);
+
+    menuList.appendChild(card);
+  });
+}
