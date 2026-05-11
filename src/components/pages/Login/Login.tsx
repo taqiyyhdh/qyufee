3017d5d4128b483c549fb5aa5fd7eff5627238de
taqiyyhdh <taqiyyhdh@gmail.com>
2026-05-11 21:58:41 +0700

    💄 style: fix text capitalization

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -26,7 +26,7 @@ const Login = () => {
       <div className={styles.card}>
         <h1 className={styles.title}>Login</h1>
         <form className={styles.form} onSubmit={handleLogin}>
-          <Input label='email' name='email' id='email' type='email' placeholder='Insert Email' required />
+          <Input label='Email' name='email' id='email' type='email' placeholder='Insert Email' required />
           <Input label='Password' name='Password' id='Password' type='Password' placeholder='Insert Password' required />
           <Button type="submit"><LogIn size={16} strokeWidth={2.5} />Login</Button>
         </form>
